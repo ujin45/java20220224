32c30bc6284fa048c2b6a918aa8d1a84e187d79d
ujin45 <99157622+ujin45@users.noreply.github.com>
2022-02-24 12:16:33 +0900

    first commit

---
+++ src/chap01/CodingTip.java
@@ -0,0 +1,26 @@
+package chap01;
+
+public class CodingTip {
+	// hint : ctrl + space
+	
+	// main 메소드 코드 단축 키워드 : main
+	public static void main(String[] args) {
+		
+		// 자동코드포맷팅 : ctrl + shift + f (들여쓰기)
+		
+		// 콘솔 출력
+		System.out.println("출력할 텍스트");
+		
+		//콘솔 출력 고드 단축 키워드 : sysout, sout
+		System.out.println("출력할 텍스트");
+		System.out.println("출력할 텍스트");
+		
+		// 실행 : ctrl + f11
+		
+		// 주석 : ctrl + /
+		
+		// 창크기 변경 : ctrl + m
+		
+		// 저장 : ctrl + s
+	}
+}
