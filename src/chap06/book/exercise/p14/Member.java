c247c9a6d066cc6f327bccd44615c607916b337d
ujin45 <99157622+ujin45@users.noreply.github.com>
2022-03-10 18:01:24 +0900

    Update Member.java

--- src/chap06/book/exercise/p14/Member.java
+++ src/chap06/book/exercise/p14/Member.java
@@ -7,8 +7,15 @@ public class Member {
 	String age;
 	
 	
-	Member(String name, String id) {
-		this.name =name;
+//	Member(String name, String id) {
+//		this.name =name;
+//		this.id = id;
+//	}
+
+
+	public Member(String name, String id) {
+		super();
+		this.name = name;
 		this.id = id;
 	}
 }
